4473d5b745692c33c98274a8b6d4a42fa468623c
anjaniaddweb <anjani.p.addweb@gmail.com>
2021-12-23 17:00:04 +0530

    weather app submitted

---
+++ weather-app/config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "49851e0a7859639bf80e7d237fd280e6"
+    }
+}
+
+module.exports = constants;
